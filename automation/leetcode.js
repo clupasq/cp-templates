96f3f9328b025425815d20054f9bd16ac550af84
Cristian Lupascu <cristi.lupascu@pm.me>
2021-11-27 19:22:25 +0200

    Fix LeetCode automation: make it work as bookmark code

--- automation/leetcode.js
+++ automation/leetcode.js
@@ -1,16 +1,16 @@
 (() => {
     const copyToClipboard = (text) => {
-        const dummy = document.createElement("textarea")
-        document.body.appendChild(dummy)
-        dummy.value = text
-        dummy.select()
-        document.execCommand("copy")
-        document.body.removeChild(dummy)
-    }
+        const dummy = document.createElement("textarea");
+        document.body.appendChild(dummy);
+        dummy.value = text;
+        dummy.select();
+        document.execCommand("copy");
+        document.body.removeChild(dummy);
+    };
 
     const getTests = () => {
-        const strongs = [...document.querySelectorAll("strong")]
-        const inputLabels = strongs.filter(s => s.textContent === "Input:")
+        const strongs = [...document.querySelectorAll("strong")];
+        const inputLabels = strongs.filter(s => s.textContent === "Input:");
         return inputLabels.map(l => {
             const input = l.nextSibling;
             const output = input.nextSibling.nextSibling;
@@ -19,45 +19,28 @@
                 output.textContent.trim()
             ]
         });
-    }
+    };
 
     const getCode = (lang="python3") => {
-        const def = pageData.codeDefinition.find(r => r.value === lang)
-        return def.defaultCode
-    }
+        const def = pageData.codeDefinition.find(r => r.value === lang);
+        return def.defaultCode;
+    };
 
     const getFunctionName = (code) => {
-        return code.split("\n")[1].trim().split(" ")[1].split("(")[0]
+        return code.split("\n")[1].trim().split(" ")[1].split("(")[0];
+    };
 
-    }
-
-    const code = getCode()
+    const code = getCode();
     const fnName = getFunctionName(code);
-    const tests = getTests()
+    const tests = getTests();
 
-    let template = `
-from typing import List
-
-${code}
-        pass
-
-lc_solution = Solution()
-`
+    let template = `\nfrom typing import List\n\n${code}        pass\n\nlc_solution = Solution()\n\n`;
 
     for (let i = 0; i < tests.length; i++) {
-        const [input, output] = tests[i]
-
-        const test = `
-def test_example_${i}():
-    actual = lc_solution.${fnName}(${input})
-    assert ${output} == actual
-
-`
+        const [input, output] = tests[i];
+        const test = `def test_example_${i}():\n    actual = lc_solution.${fnName}(${input})\n    assert ${output} == actual\n\n`;
         template += test
     }
 
-    copyToClipboard(template)
+    copyToClipboard(template);
 })()
-
-
-
